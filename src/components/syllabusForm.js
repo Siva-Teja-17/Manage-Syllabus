41e61cb91f24ca756dde33fac8d3f2005e9b9761
Siva-Teja-17 <teja_seela@tecnics.com>
2023-12-27 14:35:03 +0530

    Updated version

--- src/components/syllabusForm.js
+++ src/components/syllabusForm.js
@@ -1,55 +1,114 @@
 import React, { useState } from "react";
-import { Button, Form, Row, Col } from "react-bootstrap";
+import { Button, Form, Row, Col, Badge } from "react-bootstrap";
 import SyllabusHandle from "./syllabusHandle";
 
 const SyllabusForm = (props) => {
-    const { index, syllabusItem, closeForm, saveData } = props;
-    const [title, setTitle] = useState(syllabusItem.title || "");
-    const [description, setDescription] = useState(syllabusItem.description || "");
-    const [objectives, setObjectives] = useState(syllabusItem.objectives || "");
+  const { index, syllabusItem, closeForm, saveData } = props;
+  const [title, setTitle] = useState(syllabusItem.title || "");
+  const [description, setDescription] = useState(
+    syllabusItem.description || ""
+  );
+  const [objectives, setObjectives] = useState(syllabusItem.objectives || []);
+  const [objective, setObjective] = useState("");
 
-    const handleChange = () => {
-        const updatedSyllabus = ({ title: title, description: description, objectives: objectives, editMode: false , cancelMode: true});
-        saveData(index, updatedSyllabus);
-    }
-    return (
-        <div>
-        <Form>
-            <Form.Group as={Row} className='mb-2'>
-                <Col></Col>
-                <Col xs={1}>
-                    <Form.Label>Title</Form.Label>
-                </Col>
-                <Col xs={5}>
-                    <Form.Control type='text' placeholder='Enter syllabus title' value={title} onChange={(e) => { setTitle(e.target.value) }} />
-                </Col>
-                <Col></Col>
-            </Form.Group>
-            <Form.Group as={Row} className='mb-2'>
-                <Col></Col>
-                <Col xs={1}>
-                    <Form.Label>Description</Form.Label>
-                </Col>
-                <Col xs={5}>
-                    <Form.Control as='textarea' rows={3} type='text' placeholder='Enter syllabus description' value={description} onChange={(e) => { setDescription(e.target.value) }} />
-                </Col>
-                <Col></Col>
-            </Form.Group>
-            <Form.Group as={Row} className='mb-2'>
-                <Col></Col>
-                <Col xs={1}>
-                    <Form.Label>Outcomes</Form.Label>
-                </Col>
-                <Col xs={5}>
-                    <Form.Control type='text' placeholder='Learning outcomes' value={objectives} onChange={(e) => { setObjectives(e.target.value) }} />
-                </Col>
-                <Col></Col>
-            </Form.Group>
-            <Button variant='primary' onClick={handleChange} type='button'>Save</Button>{' '}
-            <Button variant='secondary' onClick={closeForm}>Cancel</Button>
-        </Form>
-        </div>
-    );
-}
+  const handleChange = () => {
+    const updatedSyllabus = {
+      title: title,
+      description: description,
+      objectives: objectives,
+      editMode: false,
+      cancelMode: true,
+    };
+    saveData(index, updatedSyllabus);
+  };
+
+  const addObjective = () => {
+    setObjectives([...objectives, objective]);
+    setObjective("");
+  };
+
+  const deleteObjective = (index) => {
+    const cloneObjectives = [...objectives];
+    cloneObjectives.splice(index, 1);
+    setObjectives(cloneObjectives);
+  };
+
+  return (
+    <div>
+      <Form>
+        <Form.Group as={Row} className="mb-2">
+          <Col></Col>
+          <Col xs={1}>
+            <Form.Label>Title</Form.Label>
+          </Col>
+          <Col xs={5}>
+            <Form.Control
+              type="text"
+              placeholder="Enter syllabus title"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+          </Col>
+          <Col></Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-2">
+          <Col></Col>
+          <Col xs={1}>
+            <Form.Label>Description</Form.Label>
+          </Col>
+          <Col xs={5}>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              type="text"
+              placeholder="Enter syllabus description"
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+          </Col>
+          <Col></Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-2">
+          <Col></Col>
+          <Col xs={1}>
+            <Form.Label>Outcomes</Form.Label>
+          </Col>
+          <Col xs={5}>
+            <Form.Control
+              type="text"
+              placeholder="Learning outcomes"
+              value={objective}
+              onChange={(e) => {
+                setObjective(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  addObjective();
+                }
+              }}
+            />
+            {objectives.map((outcome, index) => (
+              <Button key={index} variant="success" onClick={() => deleteObjective(index)}>
+                {outcome}
+                <Badge bg="secondary">X</Badge>
+              </Button>
+            ))}
+          </Col>
+          <Col></Col>
+        </Form.Group>
+        <Button variant="primary" onClick={handleChange}>
+          Save
+        </Button>{" "}
+        <Button variant="secondary" onClick={closeForm}>
+          Cancel
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default SyllabusForm;
--- src/components/syllabusHandle.js
+++ src/components/syllabusHandle.js
@@ -8,57 +8,80 @@ const SyllabusHandle = () => {
 
   const openForm = () => {
     const cloneSyllabusData = [...syllabusData];
-    cloneSyllabusData.push({ title: "", description: "", objectives: "", editMode: true , cancelMode: false});
+    cloneSyllabusData.push({
+      title: "",
+      description: "",
+      objectives: [],
+      editMode: true,
+      cancelMode: false,
+    });
     setSyllabusData(cloneSyllabusData);
-  }
+  };
 
-  const editForm = (index) =>
-  {
+  const editForm = (index) => {
     const cloneSyllabusData = [...syllabusData];
-    cloneSyllabusData.map((item, position) => {
+    cloneSyllabusData.forEach((item, position) => {
       if (index === position) {
         item.editMode = true;
         setSyllabusData(cloneSyllabusData);
       }
     });
-  }
+  };
 
   const closeForm = () => {
     const modifiedSyllabusCards = [...syllabusData];
     modifiedSyllabusCards.map((item, index) => {
       if (item.cancelMode == false) {
         syllabusData.splice(index, 1);
-        setSyllabusData(modifiedSyllabusCards);
       }
+      return item;
     });
-    const cloneSyllabusData = syllabusData.map((item) => ({...item, editMode: false}));
+    const cloneSyllabusData = syllabusData.map((item) => {
+      return { ...item, editMode: false };
+    });
     setSyllabusData(cloneSyllabusData);
-  }
+  };
 
   const saveData = (index, updatedSyllabus) => {
     const cloneSyllabusData = [...syllabusData];
     cloneSyllabusData[index] = updatedSyllabus;
     setSyllabusData(cloneSyllabusData);
-  }
+  };
 
   const deleteData = (index) => {
     const cloneSyllabusData = [...syllabusData];
     cloneSyllabusData.splice(index, 1);
     setSyllabusData(cloneSyllabusData);
-  }
+  };
 
   return (
     <div>
       Add Syllabus{" "}
-      <Button variant='primary' onClick={openForm}> + </Button>
+      <Button variant="primary" onClick={openForm}>
+        {" "}
+        +{" "}
+      </Button>
       {syllabusData.map((syllabusItem, index) => {
-        return (syllabusItem.editMode ?
-          (<SyllabusForm key={index} index={index} syllabusItem={syllabusItem} closeForm={closeForm} saveData={saveData} />) :
-          (<SyllabusCards key={index} index={index} syllabusItem={syllabusItem} editForm={() => editForm(index)} deleteData={() => deleteData(index)} />)
-        )
+        return syllabusItem.editMode ? (
+          <SyllabusForm
+            key={index}
+            index={index}
+            syllabusItem={syllabusItem}
+            closeForm={closeForm}
+            saveData={saveData}
+          />
+        ) : (
+          <SyllabusCards
+            key={index}
+            index={index}
+            syllabusItem={syllabusItem}
+            editForm={() => editForm(index)}
+            deleteData={() => deleteData(index)}
+          />
+        );
       })}
     </div>
   );
-}
+};
 
 export default SyllabusHandle;
